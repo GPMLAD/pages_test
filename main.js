78a135da3e7b455029652f1e2baafc04380805ca
Giovanni Perotto <giovanniperotto@hotmail.com>
2022-07-28 14:55:34 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeColor() {
+  let state = document.getElementById('modulos')
+  state.classList.remove('grey')
+
+  if (state.classList.length == 0) {
+    return
+  }
+
+  return console.log('batata')
+}
